26d5a7642ea46bb6ce9df413b273d6d68d0f26d8
Inès Derraz <inesderraz@hotmail.fr>
2022-09-05 16:12:32 +0200

    remove archive page

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
     title: 'Inès Derraz',
     description:
       'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '',
+    twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
